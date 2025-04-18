cedc2913566a5ecf141b29434e3834ff3afec53d
Ameer <ameryasen45@gmail.com>
2025-04-18 10:59:48 +0300

    chore: Update project configuration files

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/<react-test>/', // Set this to match your repo name
   
 })
